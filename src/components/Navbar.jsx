2afbf75025046d461e93283605263d3fc246eb43
Erhan <erhantezer68@gmail.com>
2022-07-12 20:44:48 +0300

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,41 @@ import styles from "./Navbar.module.css";
 
 
 
-
-
 const Navbar = () => {
   const navigate =useNavigate();
   return (
    <Container className={styles.navbar}>
-    
     <div className={styles.logo}>
-        <Link to="/home" className={styles.link}><span>Erhan's Kitchen</span> RECIEP</Link>
+        <Link 
+        to="/home" 
+        className={styles.link}>
+        <span>Erhan's Kitchen</span> 
+        RECIEP
+        </Link>
     </div>
-
     <div className={styles.buttons}>
-        <Link to="/about" className={styles.link}>ABOUT</Link>
-        <a href="https://github.com/erhantezer" rel="noopener noreferrer" target="_blank" className={styles.link}>GITHUB</a>
-        <Link to="/" className={styles.link} onClick={() =>navigate("/")}>LOGOUT</Link>
-    </div>
+        <Link 
+        to="/about" 
+        className={styles.link}>
+        ABOUT
+        </Link>
 
+        <a 
+        href="https://github.com/erhantezer" 
+        rel="noopener noreferrer" 
+        target="_blank" 
+        className={styles.link}>
+        GITHUB
+        </a>
+
+        <Link 
+        to="/" 
+        className={styles.link} 
+        onClick={() =>navigate("/")}>
+        LOGOUT
+        </Link>
+    </div>
    </Container>
   )
 }
-
 export default Navbar
